32ae2686753c38f4223355fc43263950f162c3cb
SRYian <92420947+SRYian@users.noreply.github.com>
2022-02-25 16:22:34 +0700

    Commented Registration
    Commented registration file, explaination on what everything does

--- Soal Shift_1/register.sh
+++ Soal Shift_1/register.sh
@@ -1,48 +1,38 @@
 #!/bin/bash
+
+# prepare userlog, user list data, and datetime
 fileuser=users/user.txt
 filelog=log.txt
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 
+echo "REGISTRATION";
+# Prompt to enter username
 echo "Please enter your username";
 read username;
 
-# check if user exist
+# check if user exist using grep, -q to search a specific substring
 if grep -q $username "$fileuser"; 
 then
-    # write this to file
+    # write this to file, that the user already exist
     echo "-ERROR-"
     echo "$dt REGISTER: ERROR User already exists" >> "$filelog" 
 else
+    #prompt the password     
     echo "Please enter your password";
     # use before pass to hide -s
     read -s pass;
     LEN=${#pass}
-    # echo $LEN
+    # check if it is alphanumeric, has capital and not the same with the username
     if  [[ $pass =~ [0-9] ]] && [[ $pass =~ [a-z] ]] && [[ $pass =~ [A-Z] ]] && [[ "$LEN" -ge 8 ]] && [[ $pass != $username ]]; 
     then        
-        # write this to file
+        # Write to file that the username and password has been registered
         echo "REGISTRATION SUCCESS!!"
-        echo "$dt REGISTER:INFO User $username registered successfully" >> "$filelog" 
+        echo "$dt REGISTER:INFO User $username registered successfully" >> "$filelog"
+        #UNSAFE to do in regular practice, write the username and password to a file, separated by -
+        #UNENCRYPTEDBTW
         echo "$username-$pass" >> "$fileuser"
     else
+        #If the password doesnt meet the requirement
         echo "Password must be alphanumeric and a minimum of 8 chacarters"
     fi 
 fi
-
-
-
-
-
-
-# function validatePassword ()
-# {           
-#     local stat=1    #assigns 1 to (stat)
-#     local pass=$1
-#     LEN=${#pass}    #counts each character in the password length
-#     echo $LEN       #Prints string length
-#                         #checks for nums        chesks for lowercase    checks for uppercase            checks if pass is greater than 8
-#     if  [[ $pass =~ [0-9] ]] && [[ $pass =~ [a-z] ]] && [[ $pass =~ [A-Z] ]]  && [[ "$LEN" -ge 8 ]]; then 
-#                     stat=$?         #return 1 for false
-#     fi  
-#     return $stat
-# } 
