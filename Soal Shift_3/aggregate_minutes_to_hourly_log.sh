dae3377705514a80fd1887a3bf23b7739d1eb223
SRYian <bil.lybrianto00@gmail.com>
2022-02-26 06:12:48 -0500

    commented

--- Soal Shift_3/aggregate_minutes_to_hourly_log.sh
+++ Soal Shift_3/aggregate_minutes_to_hourly_log.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 dt=$(date '+%Y%m%d%H');
+# use cat to search for a file that contains the dt string and appends it to a new file called metrics_agg
 cat /home/$USER/metrics_*"$dt"*.log > /home/$USER/metrics_agg_$dt.log
