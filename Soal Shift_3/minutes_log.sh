f28510b62f864936b302a5afbff97629890cf9d8
SRYian <bil.lybrianto00@gmail.com>
2022-02-26 08:23:42 -0500

    added 2 types: w/wo awk

--- Soal Shift_3/minutes_log.sh
+++ Soal Shift_3/minutes_log.sh
@@ -1,10 +1,23 @@
 #!/bin/bash
-ram="$(free -m)"
-disk=$(du -sh $HOME)
-#write out current crontab
-crontab -l > mycron
-#echo new cron into cron file
-echo "* * * * * free -m; du -sh $HOME; >> /home/$USER/metrics_$(date +\%Y\%m\%d\%H\%M\%S).log" >> mycron
-#install new cron file
-crontab mycron
-rm mycron
+
+# 1
+# Innate cron/auto run cron from this bash script
+# problem: date doesnt change, so it all comes into 1 file 
+
+# #write out current crontab
+# crontab -l > mycron
+# #echo new command into cron file
+
+# echo "* * * * * usr/bin/free -m >> /home/$USER/metrics_$(date +\%Y\%m\%d\%H\%M\%S).log" >> mycron
+# echo "* * * * * usr/bin/du -sh $HOME >> /home/$USER/metrics_$(date +\%Y\%m\%d\%H\%M\%S).log" >> mycron
+# #install new cron file
+# crontab mycron
+# rm mycron
+
+# 2
+# Alternative without cron
+# this uses cron externally(aka DIY :v) to run this file
+
+dt=$(date +\%Y\%m\%d\%H\%M\%S);
+free -m -t >> /home/$USER/metrics_$dt.log
+du -sh $HOME >> /home/$USER/metrics_$dt.log
